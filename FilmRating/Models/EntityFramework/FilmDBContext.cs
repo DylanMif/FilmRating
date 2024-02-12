260f4b98bb6c85f272c6da9dd952e17c74f04606
DylanMif <dylan.miftari@etu.univ-savoie.fr>
2024-02-12 11:35:59 +0100

    DatabaseContext

---
+++ FilmRating/Models/EntityFramework/FilmDBContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using FilmRating.Models.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Logging;
+
+namespace TP3Console.Models.EntityFramework
+{
+    public partial class FilmDBContext : DbContext
+    {
+        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        public FilmDBContext()
+        {
+        }
+
+        public FilmDBContext(DbContextOptions<FilmDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Utilisateur> Utilisateurs { get; set; } = null!;
+        public virtual DbSet<Notation> Notations { get; set; } = null!;
+        public virtual DbSet<Film> Films { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseLoggerFactory(MyLoggerFactory)
+                    .EnableSensitiveDataLogging()
+                    .UseNpgsql("Server=localhost;port=5432;Database=RatingFilmsDB;uid=postgres;password=postgres;");
+                /*optionsBuilder.UseLazyLoadingProxies();*/
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Notation>(entity =>
+            {
+                entity.HasKey(e => new { e.UtilisateurId, e.FilmId })
+                    .HasName("pk_notations");
+
+                entity.HasOne(n => n.UtilisateurNotant)
+                    .WithMany(u => u.NotesUtilisateur)
+                    .HasForeignKey(n => n.UtilisateurId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_notation_utilisateur");
+
+                entity.HasOne(n => n.FilmNote)
+                    .WithMany(f => f.NotesFilm)
+                    .HasForeignKey(n => n.FilmId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_notation_film");
+            });
+
+            modelBuilder.Entity<Utilisateur>().Property(e => e.DateCreation).HasDefaultValueSql("getdate()");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
