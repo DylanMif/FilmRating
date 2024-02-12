d38dd1144679682287db04d21e368681f5e7d4bb
DylanMif <dylan.miftari@etu.univ-savoie.fr>
2024-02-12 10:41:18 +0100

    Ajoutez des fichiers projet.

---
+++ FilmRating/Models/EntityFramework/Film.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FilmRating.Models.EntityFramework
+{
+    [Table("t_e_film_flm")]
+    public class Film
+    {
+        [Key]
+        [Column("flm_id")]
+        public int FilmId { get; set; }
+
+        [Column("flm_titre")]
+        [StringLength(100)]
+        public string Titre { get; set; }
+
+        [Column("flm_resume")]
+        public string Resume { get; set; }
+
+        [Column("flm_datesortie", TypeName = "date")]
+        public DateTime DateSortie { get; set; }
+
+        [Column("flm_duree", TypeName = "numeric(3,0)")]
+        public decimal Duree { get; set; }
+    }
+}
